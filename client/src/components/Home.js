c9b8d402aef80ddb1eb3ddb8f0e254995603290c
Eli <elilogbro@gmail.com>
2022-11-26 19:10:53 -0500

    created spacing for container

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,21 @@
 import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Home() {
     return (
-        <Container fluid className="d-flex justify-content-center">
+        <Container fluid className="mt-5">
             <Row>
-                <Image
-                    roundedCircle
-                    fluid
-                    src={'/me.jpg'}
-                    alt="me"
-                />
+                <Col className="text-center">
+                    <Image
+                        roundedCircle
+                        fluid
+                        src={'/me.jpg'}
+                        alt="me"
+                        width="30%"
+                    />
+                </Col>
             </Row>
         </Container>
     )
