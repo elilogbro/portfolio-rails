2eedc6fd0f3095c7e94230bd782e6f854fd71fa4
Eli <elilogbro@gmail.com>
2022-11-28 11:44:50 -0500

    navbar completed

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,5 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillPersonFill } from 'react-icons/bs';
@@ -12,62 +11,65 @@ function Navigation() {
 
     return (
         <Navbar className="navbar-custom">
-            <Nav.Item>
-                <LinkContainer to="/">
-                    <Nav.Link>
-                        <AiFillHome
-                            style={{
-                                color: 'white',
-                                height: '4vh',
-                                width: '4vw',
-                                margin: '0.7vw'
-                            }}
-                        />
-                    </Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/projects">
-                    <Nav.Link>
-                        <HiViewGrid
-                            style={{
-                                color: 'white',
-                                height: '4vh',
-                                width: '4vw',
-                                margin: '0.7vw'
-                            }}
-                        />
-                    </Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/about">
-                    <Nav.Link>
-                        <BsFillPersonFill
-                            style={{
-                                color: 'white',
-                                height: '4vh',
-                                width: '4vw',
-                                margin: '0.7vw'
-                            }}
-                        />
-                    </Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/contact">
-                    <Nav.Link>
-                        <CgPhone
-                            style={{
-                                color: 'white',
-                                height: '4vh',
-                                width: '4vw',
-                                margin: '0.7vw'
-                            }}
-                        />
-                    </Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
+            <Navbar.Brand className="brand-container">Eb</Navbar.Brand>
+            <div className="nav-container">
+                <Nav.Item>
+                    <LinkContainer to="/">
+                        <Nav.Link>
+                            <AiFillHome
+                                style={{
+                                    color: 'white',
+                                    height: '4vh',
+                                    width: '4vw',
+                                    margin: '0.7vw'
+                                }}
+                            />
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/projects">
+                        <Nav.Link>
+                            <HiViewGrid
+                                style={{
+                                    color: 'white',
+                                    height: '4vh',
+                                    width: '4vw',
+                                    margin: '0.7vw'
+                                }}
+                            />
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/about">
+                        <Nav.Link>
+                            <BsFillPersonFill
+                                style={{
+                                    color: 'white',
+                                    height: '4vh',
+                                    width: '4vw',
+                                    margin: '0.7vw'
+                                }}
+                            />
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/contact">
+                        <Nav.Link>
+                            <CgPhone
+                                style={{
+                                    color: 'white',
+                                    height: '4vh',
+                                    width: '4vw',
+                                    margin: '0.7vw'
+                                }}
+                            />
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+            </div>
         </Navbar>
     )
 }
