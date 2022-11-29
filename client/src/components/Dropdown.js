01f0de742abe7b392541bac2ff2a2187eccd62a7
Eli <elilogbro@gmail.com>
2022-11-28 21:16:06 -0500

    created popup for resume

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,15 +1,22 @@
+import React, { useState } from 'react';
 import {
     Container,
     List,
     ListItem
 } from '../styles/DropdownStyles';
+import Popup from 'reactjs-popup';
+import { ReactPDF } from '@react-pdf/renderer';
 
 function Dropdown() {
 
     return (
         <Container>
             <List>
-                <ListItem>View</ListItem>
+                <Popup trigger={<ListItem>View</ListItem>} position="right-center" modal>
+                    <object data={'/resume.pdf'} type="application/pdf" width="100%" height="100%">
+                        <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+                    </object>
+                </Popup>
                 <a
                     href={'/resume.pdf'}
                     download="EliottBrownResume"
