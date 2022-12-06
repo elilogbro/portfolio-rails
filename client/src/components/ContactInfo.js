848d5ae596f1664327f965f9ac6f9a18e8afa54a
Eli <elilogbro@gmail.com>
2022-12-06 15:49:57 -0500

    created email form and captcha

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -1,11 +1,31 @@
-
+import emailjs from 'emailjs-com';
 
 function ContactInfo() {
+
+    function sendEmail(e) {
+      e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+  
+      emailjs.sendForm('service_v3bpdl8', 'template_4nl0spn', e.target, 'EhI_xZcAYhshS9nyo')
+        .then((result) => {
+            window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+  
     return (
-        <div>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="from_name" />
+        <label>Email</label>
+        <input type="email" name="from_email" />
+        <label>Message</label>
+        <textarea name="html_message" />
+        <input type="submit" value="Send" />
+        <div class="g-recaptcha" data-sitekey="6LcOSF4jAAAAAHJx88mdVNetu4rHhViMOuVOt6nR"></div>
+      </form>
+    );
+  }
 
-        </div>
-    )
-}
-
-export default ContactInfo;
+  export default ContactInfo;
