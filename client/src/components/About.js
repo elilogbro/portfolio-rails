90754842c6adbca8c205194612147db97f85a7c0
Eli <elilogbro@gmail.com>
2022-12-07 12:49:30 -0500

    styled About component's Container, Header, and SubContainer divs

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,16 +1,25 @@
-
+import {
+    Image,
+    Container,
+    Header,
+    SubContainer
+} from '../styles/AboutStyles';
 
 function About() {
     return (
-        <div>
-            <h4>Who is Eliott Brown?</h4>
-            <p>A curious Full Stack Software Engineer who is passionate about the Frontend and is constantly trying to further himself.</p>
-            <img src={'/mewithhenry.jpg'} alt="hiking with my dog, Henry" />
-            <div>
-                <p>As a former Forestry Technician of four years, I value all things outdoors and hope to continue sharing my passion for environmental stewardship.</p>
-                <p>After finishing Flatiron School's Software Engineering program, I'm on the hunt for a role where I can put both my passion for the outdoors and technology skills to work.</p>
-            </div>
-        </div>
+        <Container>
+            <Header>
+                <h4>Who is Eliott Brown?</h4>
+                <p>A curious Full Stack Software Engineer who is passionate about the Frontend and is constantly trying to further himself.</p>
+            </Header>
+            <SubContainer>
+                <Image src={'/mewithhenry.jpg'} alt="hiking with my dog, Henry" />
+                <div>
+                    <p>As a former Forestry Technician of four years, I value all things outdoors and hope to continue sharing my passion for environmental stewardship.</p>
+                    <p>After finishing Flatiron School's Software Engineering program, I'm on the hunt for a role where I can put both my passion for the outdoors and technology skills to work.</p>
+                </div>
+            </SubContainer>
+        </Container>
     )
 }
 
--- client/src/styles/AboutStyles.js
+++ client/src/styles/AboutStyles.js
@@ -5,4 +5,19 @@ export const Image = styled.img`
     width: 50%;
 `;
 
-export 
+export const Container = styled.div`
+    margin-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    gap: 10vh;
+`;
+
+export const Header = styled.div`
+    text-align: center;
+`;
+
+export const SubContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 4vw;
+`;
