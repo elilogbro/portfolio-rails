68d97657d9a2beddf3d357dec55277579f75577a
Eli <elilogbro@gmail.com>
2022-12-07 12:41:03 -0500

    created About skeleton with image

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,13 @@
 function About() {
     return (
         <div>
-
+            <h4>Who is Eliott Brown?</h4>
+            <p>A curious Full Stack Software Engineer who is passionate about the Frontend and is constantly trying to further himself.</p>
+            <img src={'/mewithhenry.jpg'} alt="hiking with my dog, Henry" />
+            <div>
+                <p>As a former Forestry Technician of four years, I value all things outdoors and hope to continue sharing my passion for environmental stewardship.</p>
+                <p>After finishing Flatiron School's Software Engineering program, I'm on the hunt for a role where I can put both my passion for the outdoors and technology skills to work.</p>
+            </div>
         </div>
     )
 }
