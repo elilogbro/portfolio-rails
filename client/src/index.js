b2bb68158c036019d34ea289e1ee6206fcc174d2
Eli <elilogbro@gmail.com>
2022-12-14 14:14:08 -0500

    de;leted loading context

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import './custom.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { IsMobileProvider } from './context/IsMobileContext';
-import { LoadingProvider } from './context/LoadingContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <IsMobileProvider>
-      <LoadingProvider>
-        <App />
-      </LoadingProvider>
+      <App />
     </IsMobileProvider>
   </Router>
 );
