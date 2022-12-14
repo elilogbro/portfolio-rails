de51d6b14ca1708f6e7bb1eb29ad34e11871d2b9
Eli <elilogbro@gmail.com>
2022-12-14 14:22:22 -0500

    moved client out of backend repo

--- client/src/mobile-styles/MobileProjectsContainerStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding-bottom: 14vh;
-    gap: 6vh;
-    width: 100%;
-`;
--- client/src/styles/ProjectsContainerStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-flow: column;
-    margin-top: 2vh;
-`;
